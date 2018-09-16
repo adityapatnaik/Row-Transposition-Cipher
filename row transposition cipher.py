d258bdf0b8a8a4c4ad073ce63540715ca8063139
Aditya Patnaik <adityapatnaik25x@gmail.com>
2018-09-17 02:04:37 +0530

    Add files via upload

---
+++ row transposition cipher.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+import operator
+
+
+
+kei=list(input())
+text =list(input())
+text.append('\0')
+k = defaultdict(list)
+n = len(text)
+
+i = 0
+for j in range(len(kei)):
+    for i in range(j,n,len(kei)):
+        if(text[i]=='\x00'): break
+        k[kei[j]].append(text[i])    
+
+        
+    
+        
+li = []
+li = sorted(k.values())
+for i in li:
+   m = []
+   m=i
+   for j in m:
+      print(j,end='')
